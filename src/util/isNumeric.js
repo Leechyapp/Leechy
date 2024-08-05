e05bb358e27244d03243f79e0afc16ad1d24e6ec
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-05 02:41:50 -0400

    Create isNumeric.js

---
+++ src/util/isNumeric.js
@@ -0,0 +1,6 @@
+export const isNumeric = str => {
+  if (typeof str != 'string') return false; // we only process strings!
+  return (
+    !isNaN(str) && !isNaN(parseFloat(str)) // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+  ); // ...and ensure strings of whitespace fail
+};
