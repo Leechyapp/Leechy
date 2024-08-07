148149070bff06866f41ee95b603b5cb6ff269f0
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-07 16:09:44 -0400

    ToolTip component created

---
+++ src/components/ToolTip/ToolTip.js
@@ -0,0 +1,18 @@
+import { Tooltip as ReactTooltip } from 'react-tooltip';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import css from './ToolTip.module.scss';
+
+const ToolTip = props => {
+  const { content } = props;
+  return (
+    <>
+      <span className={css.toolTip}>
+        <span className={css.spacing}></span>
+        <FontAwesomeIcon data-tooltip-id="info-tooltip" icon={'fa-solid fa-circle-question'} />
+      </span>
+      <ReactTooltip id="info-tooltip" place="bottom" content={content} />
+    </>
+  );
+};
+
+export default ToolTip;
