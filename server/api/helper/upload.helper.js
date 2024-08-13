f4be98bd5a565a94665c92fd095d83df0c2eb399
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-13 18:02:22 -0400

    Create upload.helper.js

---
+++ server/api/helper/upload.helper.js
@@ -0,0 +1,28 @@
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const { uuid } = require('uuidv4');
+
+const s3 = require('../utils/s3.util');
+
+const upload = multer({
+  storage: multerS3({
+    s3,
+    bucket: process.env.AWS_BUCKET,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'bucket-owner-full-control',
+    metadata: (req, file, cb) => {
+      cb(null, {
+        fieldName: file.originalname,
+      });
+    },
+    key: (req, file, cb) => {
+      switch (file.mimetype) {
+        default:
+          cb(null, uuid() + '.jpeg');
+          break;
+      }
+    },
+  }),
+}).array('uploads[]', 5);
+
+module.exports = upload;
