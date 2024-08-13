c1b2c2a36c2cb1a2bb9077c6fe1b35f59d9033f5
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-13 18:01:00 -0400

    Create aws.service.js

---
+++ server/api/services/aws.service.js
@@ -0,0 +1,16 @@
+const { GetObjectCommand } = require('@aws-sdk/client-s3');
+const s3 = require('../utils/s3.util');
+const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
+
+const URL_EXPIRATION_TIME = 120; // in seconds
+
+class AwsService {
+  static async generatePreSignedPutUrl(filename) {
+    const command = new GetObjectCommand({
+      Bucket: process.env.AWS_BUCKET,
+      Key: filename,
+    });
+    return await getSignedUrl(s3, command, { expiresIn: URL_EXPIRATION_TIME });
+  }
+}
+module.exports = AwsService;
