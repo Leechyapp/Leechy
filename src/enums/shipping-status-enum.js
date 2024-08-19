958ce2a2e0edceaf1fb1e1367fdd56793dd9b41d
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-19 17:14:32 -0400

    Create shipping-status-enum.js

---
+++ src/enums/shipping-status-enum.js
@@ -0,0 +1,8 @@
+export const ShippingStatusEnum = {
+  ItemShippingNotStarted: 'item-shipping-not-started',
+  ItemShipped: 'item-shipped',
+  ItemReceived: 'item-received',
+  ItemReturnShipped: 'item-return-shipped',
+  ItemReturnReceived: 'item-return-received',
+  TransactionCompleted: 'transaction-completed',
+};
