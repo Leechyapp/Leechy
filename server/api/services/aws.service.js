cdb745ff0a4edc01f0b07dbdf7982f1714b3afd1
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-30 14:36:33 -0400

    Upgrade s3 pacakge

--- server/api/services/aws.service.js
+++ server/api/services/aws.service.js
@@ -1,5 +1,5 @@
 const { GetObjectCommand } = require('@aws-sdk/client-s3');
-// const s3 = require('../utils/s3.util');
+const s3 = require('../utils/s3.util');
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner');
 
 const URL_EXPIRATION_TIME = 120; // in seconds
