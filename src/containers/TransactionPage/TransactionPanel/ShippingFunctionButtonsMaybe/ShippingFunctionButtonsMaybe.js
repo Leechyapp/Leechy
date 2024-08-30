b198465e82b2684cce04bff36f8cd915ab036109
Crystal Yong <crystalyong2kx@gmail.com>
2024-08-30 15:56:02 -0400

    Update ShippingFunctionButtonsMaybe.js

--- src/containers/TransactionPage/TransactionPanel/ShippingFunctionButtonsMaybe/ShippingFunctionButtonsMaybe.js
+++ src/containers/TransactionPage/TransactionPanel/ShippingFunctionButtonsMaybe/ShippingFunctionButtonsMaybe.js
@@ -3,13 +3,17 @@ import { manageDisableScrolling } from '../../../../ducks/ui.duck';
 import css from './ShippingFunctionButtonsMaybe.module.scss';
 import { Modal, PrimaryButton } from '../../../../components';
 import { FormattedMessage } from 'react-intl';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ShippingStatusEnum } from '../../../../enums/shipping-status-enum';
+import { onRefreshTransactionEntity } from '../../TransactionPage.duck';
+import { updateShippingStatus } from '../../../../util/api';
+import { TransactionRoleEnum } from '../../../../enums/transaction-role.enum';
 
 const ShippingFunctionButtonsMaybe = props => {
   const { transactionId, isProvider, shippingStatus: currentShippingStatus } = props;
 
+  console.log(`currentShippingStatus`, currentShippingStatus);
   if (currentShippingStatus === ShippingStatusEnum.TransactionCompleted) {
     return null;
   }
@@ -18,78 +22,122 @@ const ShippingFunctionButtonsMaybe = props => {
   const onManageDisableScrolling = (componentId, disableScrolling) => {
     dispatch(manageDisableScrolling(componentId, disableScrolling));
   };
-
   const [shippingFunctionModalOpen, setShippingFunctionModalOpen] = useState(false);
-  const [shippingFunctionInProgress, setShippingFunctionInProgres] = useState(false);
+  const [shippingFunctionInProgress, setShippingFunctionInProgress] = useState(false);
+  const [shippingFunctionRole, setShippingFunctionRole] = useState();
+  const [nextTransition, setNextTransition] = useState();
+  const [nextTransitionButton, setNextTransitionButton] = useState();
 
-  const onShippingFunctionTransition = () => {
-    let nextTransition;
+  useEffect(() => {
+    let role;
+    let transition;
+    let transitionButton;
     switch (currentShippingStatus) {
       case ShippingStatusEnum.ItemShippingNotStarted:
       case undefined:
       case null:
-        nextTransition = ShippingStatusEnum.ItemReturnReceived;
+        role = TransactionRoleEnum.Provider;
+        transition = ShippingStatusEnum.ItemShipped;
+        transitionButton = 'markItemShipped';
         break;
-      case ShippingStatusEnum.ItemReturnShipped:
-        nextTransition = ShippingStatusEnum.ItemReceived;
+      case ShippingStatusEnum.ItemShipped:
+        role = TransactionRoleEnum.Customer;
+        transition = ShippingStatusEnum.ItemReceived;
+        transitionButton = 'markItemReceived';
         break;
       case ShippingStatusEnum.ItemReceived:
-        nextTransition = ShippingStatusEnum.ItemReturnShipped;
+        role = TransactionRoleEnum.Customer;
+        transition = ShippingStatusEnum.ItemReturnShipped;
+        transitionButton = 'markItemReturnShipped';
         break;
       case ShippingStatusEnum.ItemReturnShipped:
-        nextTransition = ShippingStatusEnum.ItemReturnReceived;
+        role = TransactionRoleEnum.Provider;
+        transition = ShippingStatusEnum.ItemReturnReceived;
+        transitionButton = 'markItemReceivedShipped';
         break;
       case ShippingStatusEnum.ItemReturnReceived:
-        nextTransition = ShippingStatusEnum.TransactionCompleted;
+        transition = ShippingStatusEnum.TransactionCompleted;
         break;
     }
+    setShippingFunctionRole(role);
+    setNextTransition(transition);
+    setNextTransitionButton(transitionButton);
+  }, [currentShippingStatus]);
 
+  const onShippingFunctionTransition = () => {
     if (nextTransition) {
-      setShippingFunctionInProgres(true);
+      setShippingFunctionInProgress(true);
+      updateShippingStatus({ transactionId: transactionId.uuid, nextTransition })
+        .then(res => {
+          setShippingFunctionInProgress(false);
+          // dispatch(onRefreshTransactionEntity(transactionId));
+          console.log(`updateShippingStatus`, res);
+          location.reload();
+        })
+        .catch(err => {
+          setShippingFunctionInProgress(false);
+          console.error(`updateShippingStatus`, err);
+        });
     }
   };
 
+  if (isProvider && shippingFunctionRole !== TransactionRoleEnum.Provider) {
+    return null;
+  }
+
+  if (!isProvider && shippingFunctionRole !== TransactionRoleEnum.Customer) {
+    return null;
+  }
+
   return (
-    <div className={css.shippingFunctionContainer}>
-      <div className={css.rowUnsetMarginLR}>
-        <div className={css.col12}>
+    shippingFunctionRole &&
+    nextTransition &&
+    nextTransitionButton && (
+      <div className={css.shippingFunctionContainer}>
+        <div className={css.rowUnsetMarginLR}>
+          <div className={css.col12}>
+            <PrimaryButton
+              onClick={() => {
+                setShippingFunctionModalOpen(true);
+              }}
+              className={css.shippingFunctionButton}
+              type="button"
+            >
+              <FormattedMessage
+                id={`ShippingFunctionButtonsMaybe.shippingFunctionModalButton.${nextTransitionButton}`}
+              />
+            </PrimaryButton>
+            <p className={css.shippingFunctionNote}>
+              <FontAwesomeIcon icon="fa-regular fa-bell" />
+              <span>
+                <FormattedMessage
+                  id={`ShippingFunctionButtonsMaybe.shippingFunctionNote.${nextTransitionButton}`}
+                />
+              </span>
+            </p>
+          </div>
+        </div>
+        <Modal
+          id="ShippingFunctionButtonsMaybe.shippingFunctionModal"
+          isOpen={shippingFunctionModalOpen}
+          onClose={() => setShippingFunctionModalOpen(false)}
+          usePortal={true}
+          onManageDisableScrolling={onManageDisableScrolling}
+        >
+          <h4>
+            <FormattedMessage id="ShippingFunctionButtonsMaybe.confirmMessage" />
+          </h4>
           <PrimaryButton
-            onClick={() => {
-              setShippingFunctionModalOpen(true);
-            }}
             className={css.shippingFunctionButton}
+            inProgress={shippingFunctionInProgress}
+            onClick={() => onShippingFunctionTransition()}
             type="button"
           >
-            <FormattedMessage id="ShippingFunctionButtonsMaybe.shippingFunctionModalButton.markItemShipped" />
+            <FormattedMessage id="ShippingFunctionButtonsMaybe.confirmShippingFunctionButton" />
           </PrimaryButton>
-          <p className={css.shippingFunctionNote}>
-            <FontAwesomeIcon icon="fa-regular fa-bell" />
-            <span>
-              <FormattedMessage id="ShippingFunctionButtonsMaybe.shippingFunctionNote.markItemShipped" />
-            </span>
-          </p>
-        </div>
+        </Modal>
       </div>
-      <Modal
-        id="ShippingFunctionButtonsMaybe.shippingFunctionModal"
-        isOpen={shippingFunctionModalOpen}
-        onClose={() => setShippingFunctionModalOpen(false)}
-        usePortal={true}
-        onManageDisableScrolling={onManageDisableScrolling}
-      >
-        <h4>
-          <FormattedMessage id="ShippingFunctionButtonsMaybe.confirmMessage" />
-        </h4>
-        <PrimaryButton
-          className={css.shippingFunctionButton}
-          inProgress={shippingFunctionInProgress}
-          onClick={() => onShippingFunctionTransition()}
-          type="button"
-        >
-          <FormattedMessage id="ShippingFunctionButtonsMaybe.confirmShippingFunctionButton" />
-        </PrimaryButton>
-      </Modal>
-    </div>
+    )
   );
 };
 export default ShippingFunctionButtonsMaybe;
