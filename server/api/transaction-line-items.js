88e29a7987e6aa0f646d08ed03d873c16d70af6c
Crystal Yong <crystalyong2kx@gmail.com>
2024-09-02 15:57:09 -0400

    transaction-line-items.js - return estimatedTrxProtectedData

--- server/api/transaction-line-items.js
+++ server/api/transaction-line-items.js
@@ -1,6 +1,8 @@
 const { transactionLineItems } = require('../api-util/lineItems');
 const { getSdk, handleError, serialize, fetchCommission } = require('../api-util/sdk');
 const { constructValidLineItems } = require('../api-util/lineItemHelpers');
+const isNumeric = require('./utils/isNumeric');
+const InsuranceMethodEnum = require('./enums/insurance-method.enum');
 
 module.exports = (req, res) => {
   const { isOwnListing, listingId, orderData } = req.body;
@@ -29,10 +31,28 @@ module.exports = (req, res) => {
       // to add some attributes like lineTotal and reversal that Marketplace API also adds to the response.
       const validLineItems = constructValidLineItems(lineItems);
 
+      const insuranceMethod = orderData.insuranceMethod;
+      let security_deposit = null;
+      if (insuranceMethod === InsuranceMethodEnum.SecurityDeposit) {
+        security_deposit = listing?.attributes?.publicData?.security_deposit;
+      }
+      let estimatedTrxProtectedData = {};
+      if (security_deposit) {
+        const securityDepositPercentageValue = isNumeric(security_deposit)
+          ? parseInt(security_deposit)
+          : null;
+        if (securityDepositPercentageValue) {
+          estimatedTrxProtectedData = {
+            insuranceMethod,
+            securityDepositPercentageValue,
+          };
+        }
+      }
+
       res
         .status(200)
         .set('Content-Type', 'application/transit+json')
-        .send(serialize({ data: validLineItems }))
+        .send(serialize({ lineItems: validLineItems, estimatedTrxProtectedData }))
         .end();
     })
     .catch(e => {
