7c2119686636ccaeea66d9498cee416404d3456b
Crystal Yong <crystalyong2kx@gmail.com>
2024-09-02 15:57:50 -0400

    initiate-privileged.js - calculateSecurityDeposit and store in protectedData

--- server/api/initiate-privileged.js
+++ server/api/initiate-privileged.js
@@ -6,12 +6,16 @@ const {
   serialize,
   fetchCommission,
 } = require('../api-util/sdk');
+const InsuranceMethodEnum = require('./enums/insurance-method.enum');
+const AccountingUtil = require('./utils/accounting.util');
+const isNumeric = require('./utils/isNumeric');
 
 module.exports = (req, res) => {
   const { isSpeculative, orderData, bodyParams, queryParams } = req.body;
 
   const sdk = getSdk(req, res);
   let lineItems = null;
+  let security_deposit = null;
 
   const listingPromise = () => sdk.listings.show({ id: bodyParams?.params?.listingId });
 
@@ -29,6 +33,11 @@ module.exports = (req, res) => {
         providerCommission,
         customerCommission
       );
+
+      const insuranceMethod = bodyParams.params.insuranceMethod;
+      if (insuranceMethod === InsuranceMethodEnum.SecurityDeposit) {
+        security_deposit = listing?.attributes?.publicData?.security_deposit;
+      }
 
       return getTrustedSdk(req);
     })
@@ -51,6 +60,26 @@ module.exports = (req, res) => {
     })
     .then(apiResponse => {
       const { status, statusText, data } = apiResponse;
+
+      if (security_deposit) {
+        const payinTotal = data?.data?.attributes?.payinTotal;
+        if (payinTotal) {
+          const securityDepositPercentageValue = isNumeric(security_deposit)
+            ? parseInt(security_deposit)
+            : null;
+          if (securityDepositPercentageValue) {
+            const securityDepositAmounts = AccountingUtil.calculateSecurityDeposit({
+              securityDepositPercentageValue,
+              payinTotal,
+            });
+            data.data.attributes.protectedData = {
+              ...data.data.attributes.protectedData,
+              ...securityDepositAmounts,
+            };
+          }
+        }
+      }
+
       res
         .status(status)
         .set('Content-Type', 'application/transit+json')
