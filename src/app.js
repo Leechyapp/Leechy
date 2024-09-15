37f0848a09104a4c1e983d6552c2fc64ae240212
Crystal <crystalyong2kx@gmail.com>
2024-09-15 12:18:21 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -287,22 +287,6 @@ export const ClientApp = props => {
     initNativePlatform();
   }, []);
 
-  const splashScreenInit = async () => {
-    // Hide the splash (you should do this on app launch)
-    await SplashScreen.hide();
-
-    // Show the splash for an indefinite amount of time:
-    await SplashScreen.show({
-      autoHide: false,
-    });
-
-    // Show the splash for two seconds and then automatically hide it:
-    await SplashScreen.show({
-      showDuration: 2000,
-      autoHide: true,
-    });
-  }
-
   return (
     <Configurations appConfig={appConfig}>
       <IntlProvider
