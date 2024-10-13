ec08e7257ebfe61633169741783a483fd931e1e4
cydeveloper <crystal@cymarketplaces.com>
2024-10-13 19:15:43 -0400

    Update auth.middleware.js

--- server/api/middlewares/auth.middleware.js
+++ server/api/middlewares/auth.middleware.js
@@ -1,11 +1,21 @@
 const SharetribeService = require('../services/sharetribe.service');
 const HttpException = require('../classes/http-exception.class');
+const UserService = require('../services/user.service');
+const UserSyncService = require('../services/user-sync.service');
 
 exports.authMiddleware = async (req, res, next) => {
   try {
     const currentUser = await SharetribeService.getCurrentUser(req, res);
     if (currentUser?.data?.id?.uuid) {
+      req.currentUser = currentUser.data;
       req.userUUID = currentUser.data.id.uuid;
+      const userId = await UserService.searchIdWithUUID(req.userUUID);
+      if (userId) {
+        req.userId = userId;
+      } else {
+        const newUser = await UserSyncService.insert(req.currentUser, req.userUUID);
+        req.userId = newUser[0];
+      }
       next();
     } else {
       next(new HttpException(401, 'Not authorized'));
