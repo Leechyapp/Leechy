815c94cfb9d4bcf9d6f4c0e7ffd0ed1ab732eeaa
cydeveloper <crystal@cymarketplaces.com>
2024-10-13 19:16:51 -0400

    Create Leechy.sql

---
+++ server/api/database/sql/Leechy.sql
@@ -0,0 +1,21 @@
+use Leechy;
+
+DROP TABLE IF EXISTS User;
+
+CREATE TABLE User (
+    id int NOT NULL AUTO_INCREMENT,
+    sharetribeUUID varchar(255) NOT NULL,
+    UNIQUE KEY uk_uuid (sharetribeUUID),
+    PRIMARY KEY (id)
+);
+
+DROP TABLE IF EXISTS Follows;
+
+CREATE TABLE Follows (
+    id int NOT NULL AUTO_INCREMENT,
+	followedUserId int NOT NULL,
+    followingUserId int NOT NULL,
+    created datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    UNIQUE KEY uk_followedUserId_followingUserId (followedUserId, followingUserId),
+    PRIMARY KEY (id)
+);
