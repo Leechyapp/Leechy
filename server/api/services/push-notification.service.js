16f3ad0f251354b9246a741890336c631c9d0ed1
cydeveloper <crystal@cymarketplaces.com>
2024-10-22 14:49:35 -0400

    Update push-notification.service.js

--- server/api/services/push-notification.service.js
+++ server/api/services/push-notification.service.js
@@ -1,14 +1,16 @@
+const FirebaseAdmin = require('../modules/firebase-admin');
+
 class PushNotificationService {
-  // static async sendPushNotification(messageBody) {
-  //   FirebaseAdmin.messaging()
-  //     .send(messageBody)
-  //     .then(result => {
-  //       console.log(`Success: Message Push Notification Sent:`, result);
-  //     })
-  //     .catch(error => {
-  //       console.log(`Error: Push Notification Not Sent:`, error);
-  //     });
-  // }
+  static async sendPushNotification(messageBody) {
+    FirebaseAdmin.messaging()
+      .send(messageBody)
+      .then(result => {
+        console.log(`Success: Message Push Notification Sent:`, result);
+      })
+      .catch(error => {
+        console.log(`Error: Push Notification Not Sent:`, error);
+      });
+  }
 }
 
 module.exports = PushNotificationService;
