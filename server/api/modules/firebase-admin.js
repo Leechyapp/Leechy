5a35cb794a1cae9f84d9e2d061f5d4ba2b134e89
cydeveloper <crystal@cymarketplaces.com>
2024-10-22 15:59:49 -0400

    Update firebase-admin.js

--- server/api/modules/firebase-admin.js
+++ server/api/modules/firebase-admin.js
@@ -1,7 +1,10 @@
-const FirebaseAdmin = require('firebase-admin/app');
+const FirebaseAdmin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = path.resolve(__dirname, `./service-account-${process.env.NODE_ENV}.json`);
+const serviceAccount = path.resolve(
+  __dirname,
+  `./service-account-${process.env.REACT_APP_ENV}.json`
+);
 
 FirebaseAdmin.initializeApp({
   credential: FirebaseAdmin.credential.cert(serviceAccount),
