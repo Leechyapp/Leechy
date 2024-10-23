2fa9af1fb2cd3f76e7d6374750b904f93b2a1554
cydeveloper <crystal@cymarketplaces.com>
2024-10-23 00:01:22 -0400

    Create push-notification-code.enum.js

---
+++ src/enums/push-notification-code.enum.js
@@ -0,0 +1,9 @@
+export const PushNotificationCodeEnum = {
+  BookingAccepted: 'booking-accepted',
+  BookingDeclined: 'booking-declined',
+  BookingRequested: 'booking-request',
+  BookingPayoutDetails: 'booking-payout-details',
+  Message: 'message',
+  ReviewByCustomer: 'review-by-customer',
+  ReviewByProvider: 'review-by-provider',
+};
