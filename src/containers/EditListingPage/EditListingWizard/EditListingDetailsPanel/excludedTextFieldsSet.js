a7fba290a2643bfd0b1045bf4a92e460cd35c7c5
cydeveloper <crystal@cymarketplaces.com>
2024-11-05 01:44:57 -0500

    excludedTextFieldsSet created - to exclude category generated fields

---
+++ src/containers/EditListingPage/EditListingWizard/EditListingDetailsPanel/excludedTextFieldsSet.js
@@ -0,0 +1,15 @@
+const excludedTextFieldsSet = new Set();
+
+excludedTextFieldsSet.add('womenswear');
+excludedTextFieldsSet.add('menswear');
+excludedTextFieldsSet.add('sports');
+excludedTextFieldsSet.add('toolsmachinery');
+excludedTextFieldsSet.add('furniture');
+excludedTextFieldsSet.add('toysgames');
+excludedTextFieldsSet.add('transportation');
+excludedTextFieldsSet.add('books');
+excludedTextFieldsSet.add('workout');
+excludedTextFieldsSet.add('electronics');
+excludedTextFieldsSet.add('party');
+
+export default excludedTextFieldsSet;
