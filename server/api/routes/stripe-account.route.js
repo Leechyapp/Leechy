bb4639683dda1ddcbad5a5600d4922c48f06855f
cydeveloper <crystal@cymarketplaces.com>
2024-11-17 18:07:12 -0500

    Update stripe-account.route.js

--- server/api/routes/stripe-account.route.js
+++ server/api/routes/stripe-account.route.js
@@ -6,6 +6,31 @@ class StripeAccountRoute extends BaseRoute {
   constructor(router) {
     super('stripe-account');
     router.post(
+      this.ROOT_PATH + '/retrieve-stripe-account',
+      authMiddleware,
+      StripeAccountController.retrieveStripeAccount
+    );
+    router.post(
+      this.ROOT_PATH + '/connect-stripe-account',
+      authMiddleware,
+      StripeAccountController.connectStripeAccount
+    );
+    router.post(
+      this.ROOT_PATH + '/create-stripe-account-dashboard-link',
+      authMiddleware,
+      StripeAccountController.createStripeDashboardLink
+    );
+    router.post(
+      this.ROOT_PATH + '/get-balance',
+      authMiddleware,
+      StripeAccountController.getStripeAccountBalance
+    );
+    router.post(
+      this.ROOT_PATH + '/create-payout',
+      authMiddleware,
+      StripeAccountController.createStripeAccountPayout
+    );
+    router.post(
       this.ROOT_PATH + '/update-payout-settings',
       authMiddleware,
       StripeAccountController.updatePayoutSettings
