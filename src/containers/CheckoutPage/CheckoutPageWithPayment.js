1022ab2f18fc3673555168f4e7488fdf83221b84
cydeveloper <crystal@cymarketplaces.com>
2024-11-18 13:27:44 -0500

    Update CheckoutPageWithPayment.js

--- src/containers/CheckoutPage/CheckoutPageWithPayment.js
+++ src/containers/CheckoutPage/CheckoutPageWithPayment.js
@@ -11,7 +11,16 @@ import { isTransactionInitiateListingNotFoundError } from '../../util/errors';
 import { getProcess, isBookingProcessAlias } from '../../transactions/transaction';
 
 // Import shared components
-import { H3, H4, Heading, IconSpinner, NamedLink, OrderBreakdown, Page } from '../../components';
+import {
+  H3,
+  H4,
+  Heading,
+  IconSpinner,
+  NamedLink,
+  OrderBreakdown,
+  Page,
+  PrimaryButton,
+} from '../../components';
 
 import {
   bookingDatesMaybe,
