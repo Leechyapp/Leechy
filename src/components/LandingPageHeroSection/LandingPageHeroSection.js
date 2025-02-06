f619aa7586b9ec11881c2ee8bb51670499e2fe2d
cydeveloper <crystal@cymarketplaces.com>
2025-02-06 14:57:03 -0500

    LandingPageHeroSection - design updates / redirectToSearchPage function

--- src/components/LandingPageHeroSection/LandingPageHeroSection.js
+++ src/components/LandingPageHeroSection/LandingPageHeroSection.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import css from './LandingPageHeroSection.module.scss';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 const LandingPageHeroSection = () => {
+  const history = useHistory();
   const redirectToSearchPage = () => {
-    console.log('click');
+    console.log('redirectToSearchPage clicked');
+    let searchQuery = '/s';
+    history.push(searchQuery);
   };
 
   return (
@@ -12,27 +16,27 @@ const LandingPageHeroSection = () => {
         <div className={css.overlapGroup}>
           <div className={css.searchForm}>
             <div className={css.watermark}>Leechy</div>
-            <div className={css.overlap2}>
-              <div className={css.textWrapper2}>Location</div>
+            <div className={css.locationWrapper}>
+              <div className={css.locationPlaceholder}>Location</div>
               <img
                 className={css.vector}
                 alt="Vector"
                 src="https://c.animaapp.com/x4t9OtXc/img/vector-1.svg"
               />
             </div>
-            <div className={css.overlap3}>
-              <div className={css.textWrapper2}>Dates</div>
+            <div className={css.datesWrapper}>
+              <div className={css.datesPlaceholder}>Dates</div>
               <img
                 className={css.img}
                 alt="Vector"
                 src="https://c.animaapp.com/x4t9OtXc/img/vector.svg"
               />
             </div>
-            <div className={css.divWrapper}>
-              <div className={css.textWrapper3}>Category</div>
+            <div className={css.categoryDropdownWrapper}>
+              <div className={css.categoryPlaceholder}>Category</div>
             </div>
-            <div className={css.searchButton}>
-              <div className={css.textWrapper4}>Search</div>
+            <div className={css.searchButton} onClick={() => redirectToSearchPage()}>
+              <div className={css.searchButtonText}>Search</div>
             </div>
           </div>
         </div>
